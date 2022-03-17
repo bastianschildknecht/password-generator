3b3cbd7091002ad73a4307048ddc242358fefeb6
Bastian Schildknecht <bastian.schildknecht@gmail.com>
2022-03-17 23:03:49 +0100

    Refactor code, add quiet mode and update README

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Password Generator
-Generates a random password with the given length and parameters.
+The friendly password generator.
+
+## Introduction
+Password Generator is a simple but powerful password generation tool. It is designed to be easy to use and easy to understand.
+It can be used for generating passwords for logins, pin codes, and much more.
+It even has a handy quiet mode, which will not display any messages but the generated password. 
+This can for example be useful when piping the output to other programs or in automation scripts.
 
 ## Usage
 ```console
 $ python generate_password.py -h
-usage: generate_password.py [-h] [-u] [-l] [-n] [-s] length
+usage: generate_password.py [-h] [-u] [-l] [-n] [-s] [-q] length
 
 generates a random password with the given length and parameters
 
@@ -17,20 +23,37 @@ optional arguments:
   -l, --lower-case  include lower case characters
   -n, --numbers     include numbers
   -s, --symbols     include symbols
+  -q, --quiet       only print password
 ```
 
-## Example
+## Examples
+Here we generate a password with 14 characters and default parameters:
 ```console
-$ python generate_password.py 10
+$ python generate_password.py 14
 
 PASSWORD GENERATOR
 ==================
 No characters specified. Defaulting to upper case, lower case, numbers, and symbols.
-With the given parameters, the generated password is one of 3.7 quintillion possible passwords.
-It will take an attacker approx. 118 years to brute force the password.
+With the given parameters, the generated password is one of 100.6 septillion possible passwords.
+It will take an attacker approx. 3.2 billion years to brute force the password.
 It may be much less with more sophisticated attacks (e.g. rainbow tables).
 
 The generated password is:
-L$BIV4ZCOl
+td$O8gDxqjYT8R
+
+```
+
+And here we generate a 6 digit pin code for a credit card:
+```console
+$ python generate_password.py 6 --numbers
+
+PASSWORD GENERATOR
+==================
+With the given parameters, the generated password is one of 1.0 million possible passwords.
+It will take an attacker approx. a moment to brute force the password.
+It may be much less with more sophisticated attacks (e.g. rainbow tables).
+
+The generated password is:
+176812
 
 ```
--- src/generate_password.py
+++ src/generate_password.py
@@ -5,108 +5,140 @@ import argparse
 import humanize
 from colorama import Fore, Style
 
-UPPER_CASE_CHARS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-LOWER_CASE_CHARS = list("abcdefghijklmnopqrstuvwxyz")
-NUMBER_CHARS = list("0123456789")
-SPECIAL_CHARS = list("!@#$%^&*()")
 
-SECONDS_IN_A_MINUTE = 60
-SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * 60
-SECONDS_IN_A_DAY = SECONDS_IN_AN_HOUR * 24
-SECONDS_IN_A_WEEK = SECONDS_IN_A_DAY * 7
-SECONDS_IN_A_YEAR = SECONDS_IN_A_DAY * 365
+UPPER_CASE_CHARS = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+LOWER_CASE_CHARS = set("abcdefghijklmnopqrstuvwxyz")
+NUMBER_CHARS = set("0123456789")
+SYMBOL_CHARS = set("!@#$%^&*()")
 
-AGE_OF_THE_UNIVERSE = 13800000000   # 13.8 billion years old
+SECONDS_IN_A_YEAR = 365 * 24 * 60 * 60
+AGE_OF_THE_UNIVERSE_Y = 13800000000   # 13.8 billion years old
 
 POTENTIAL_BF_ATTACKS_PER_SECOND = 1000000000    # 1 billion attempts per second
 
-def random_char(upper_case: bool, lower_case: bool, numbers: bool, symbols: bool) -> str:
-    """
-    Generates a random character based on the given parameters.
-    """
-    # Create a list of all the characters that can be used
-    chars = []
-    if upper_case:
-        chars.extend(UPPER_CASE_CHARS)
-    if lower_case:
-        chars.extend(LOWER_CASE_CHARS)
-    if numbers:
-        chars.extend(NUMBER_CHARS)
-    if symbols:
-        chars.extend(SPECIAL_CHARS)
 
-    # Generate a random character from the list
-    return secrets.choice(chars)
+
+def assemble_charset(upper_case: bool, lower_case: bool, numbers: bool, symbols: bool) -> list:
+    """
+    Assembles the character set based on the given parameters.
+    
+    Args:
+        upper_case: Whether to include upper case characters.
+        lower_case: Whether to include lower case characters.
+        numbers: Whether to include numbers.
+        symbols: Whether to include symbols.
+
+    Returns:
+        A list of characters contained in the character set.
+    """
+    # Create an empty set to store the character set
+    charset = set()
+
+    # Add the characters to the set
+    if upper_case:
+        charset.update(UPPER_CASE_CHARS)
+    if lower_case:
+        charset.update(LOWER_CASE_CHARS)
+    if numbers:
+        charset.update(NUMBER_CHARS)
+    if symbols:
+        charset.update(SYMBOL_CHARS)
+
+    # Return the character set
+    return list(charset)
+
+
 
 def generate_password(length: int, upper_case: bool, lower_case: bool, numbers: bool, symbols: bool) -> str:
     """
     Generates a random character sequence based on the given parameters.
+
+    Args:
+        length: The length of the character sequence to generate.
+        upper_case: Whether to include upper case characters.
+        lower_case: Whether to include lower case characters.
+        numbers: Whether to include numbers.
+        symbols: Whether to include symbols.
+    
+    Returns:
+        A random character sequence of the given length based on the given parameters.
     """
+
+    # Construct the character set based on the given parameters
+    charset = assemble_charset(upper_case, lower_case, numbers, symbols)
+
     # Create an empty string to store the generated characters
     password = ""
 
     # Generate a random character for each position in the sequence
     for _ in range(length):
-        password += random_char(upper_case, lower_case, numbers, symbols)
+        password += secrets.choice(charset)
 
-    # Return the generated password
     return password
 
-def generate_strong_password(length: int) -> str:
-    """
-    Generates a strong password with the given length.
-    The password will contain upper case, lower case, numbers, and symbols.
-    """
-    # Generate a strong password
-    password = generate_password(length, True, True, True, True)
 
-    # Return the generated password
-    return password
 
-def compute_possibilites(length: int, upper_case: bool, lower_case: bool, numbers: bool, symbols: bool) -> float:
+def compute_possibilities(length: int, upper_case: bool, lower_case: bool, numbers: bool, symbols: bool) -> float:
     """
-    Computes the number of possible passwords with the given parameters.
+    Computes the size of the password space based on the given parameters.
+
+    Args:
+        length: The length of the password.
+        upper_case: Whether to include upper case characters.
+        lower_case: Whether to include lower case characters.
+        numbers: Whether to include numbers.
+        symbols: Whether to include symbols.
+
+    Returns:
+        The size of the password space in terms of the number of possible passwords.
     """
     # Compute the number of possible characters
-    possibilities = 0
+    possible_chars = 0
     if upper_case:
-        possibilities += len(UPPER_CASE_CHARS)
+        possible_chars += len(UPPER_CASE_CHARS)
     if lower_case:
-        possibilities += len(LOWER_CASE_CHARS)
+        possible_chars += len(LOWER_CASE_CHARS)
     if numbers:
-        possibilities += len(NUMBER_CHARS)
+        possible_chars += len(NUMBER_CHARS)
     if symbols:
-        possibilities += len(SPECIAL_CHARS)
+        possible_chars += len(SYMBOL_CHARS)
 
-    possibilities = possibilities ** length
+    possibilities = possible_chars ** length
 
-    # Return the number of possibilities
     return possibilities
+
+
 
 def get_timedelta(seconds: int) -> timedelta:
     """
-    Converts seconds to a timedelta.
+    Converts seconds to a timedelta object.
+
+    Args:
+        seconds: The number of seconds to convert to a timedelta object.
+    
+    Returns:
+        A timedelta object representing the given number of seconds if the number of seconds is not too large.
+        The biggest timedelta object otherwise.
     """
     # Check if the number is too large to be converted to a timedelta
     if seconds > timedelta.max.total_seconds():
         return timedelta.max
 
-    # Calculate the number of days, hours, minutes, and seconds
-    weeks = seconds // SECONDS_IN_A_WEEK
-    seconds -= weeks * SECONDS_IN_A_WEEK
-    days = seconds // SECONDS_IN_A_DAY
-    seconds -= days * SECONDS_IN_A_DAY
-    hours = seconds // SECONDS_IN_AN_HOUR
-    seconds -= hours * SECONDS_IN_AN_HOUR
-    minutes = seconds // SECONDS_IN_A_MINUTE
-    seconds -= minutes * SECONDS_IN_A_MINUTE
-
     # Return the timedelta
-    return timedelta(weeks=weeks, days=days, hours=hours, minutes=minutes, seconds=seconds)
+    return timedelta(seconds=seconds)
+
+
 
 def get_natural_time_string(seconds: int) -> str:
     """
     Returns a natural time string given the number of seconds.
+
+    Args:
+        seconds: The number of seconds to convert to a natural time string.
+
+    Returns:
+        A human readable string representing the given number of seconds.
+        If possible, the time duration is converted to a more relatable time duration.
     """
     # Check if the number is too large to be converted to a timedelta
     if seconds > timedelta.max.total_seconds():
@@ -114,15 +146,30 @@ def get_natural_time_string(seconds: int) -> str:
         years = seconds // SECONDS_IN_A_YEAR
 
         # Check if the number is small enough to be stated in years
-        if years > AGE_OF_THE_UNIVERSE:
+        if years > AGE_OF_THE_UNIVERSE_Y:
             # The number is too large to be stated in years -> state in terms of the age of the universe
-            aou = years // AGE_OF_THE_UNIVERSE
+            aou = years // AGE_OF_THE_UNIVERSE_Y
             return "%s times the age of the universe" % humanize.intword(aou)
         else:
             # Return the time in terms of years
             return "%s years" % humanize.intword(years)
     
     return humanize.naturaldelta(get_timedelta(seconds))
+
+
+
+def optional_print(string: str, suppress: bool = False) -> None:
+    """
+    Prints the given string if the given boolean is True.
+
+    Args:
+        string: The string to print.
+        suppress: Whether to suppress the string. Defaults to False.
+    """
+    if not suppress:
+        print(string)
+
+
 
 def main(argc: int, argv: list) -> None:
     """
@@ -136,16 +183,17 @@ def main(argc: int, argv: list) -> None:
     parser.add_argument("-l", "--lower-case", action="store_true", help="include lower case characters")
     parser.add_argument("-n", "--numbers", action="store_true", help="include numbers")
     parser.add_argument("-s", "--symbols", action="store_true", help="include symbols")
+    parser.add_argument("-q", "--quiet", action="store_true", help="only print password")
     args = parser.parse_args(argv[1:])
 
     # Check if the length is valid
     if args.length < 1:
-        print("The length must be at least 1.")
+        optional_print("The length must be at least 1.", args.quiet)
         sys.exit(1)
     
     # Print header
-    print("\nPASSWORD GENERATOR")
-    print("==================")
+    optional_print("\nPASSWORD GENERATOR", args.quiet)
+    optional_print("==================", args.quiet)
 
     # If no characters are specified, default to upper case, lower case, numbers, and symbols
     if not args.upper_case and not args.lower_case and not args.numbers and not args.symbols:
@@ -153,17 +201,24 @@ def main(argc: int, argv: list) -> None:
         args.lower_case = True
         args.numbers = True
         args.symbols = True
-        print("No characters specified. Defaulting to upper case, lower case, numbers, and symbols.")
+        optional_print("No characters specified. Defaulting to upper case, lower case, numbers, and symbols.", args.quiet)
 
-    # Print information about the strength of the password
-    possibilities = compute_possibilites(args.length, args.upper_case, args.lower_case, args.numbers, args.symbols)
-    print("With the given parameters, the generated password is one of %s possible passwords." % (humanize.intword(possibilities)))
-    print("It will take an attacker approx. %s to brute force the password." % (get_natural_time_string(possibilities // POTENTIAL_BF_ATTACKS_PER_SECOND)))
-    print("It may be much less with more sophisticated attacks (e.g. rainbow tables).\n")
+    if (not args.quiet):
+        # Print information about the strength of the password
+        possibilities = compute_possibilities(args.length, args.upper_case, args.lower_case, args.numbers, args.symbols)
+        print("With the given parameters, the generated password is one of %s possible passwords." % (humanize.intword(possibilities)))
+        print("It will take an attacker approx. %s to brute force the password." % (get_natural_time_string(possibilities // POTENTIAL_BF_ATTACKS_PER_SECOND)))
+        print("It may be much less with more sophisticated attacks (e.g. rainbow tables).\n")
 
     # Generate the password
     password = generate_password(args.length, args.upper_case, args.lower_case, args.numbers, args.symbols)
-    print("The generated password is: " + Fore.GREEN+ "\n%s\n" % (password) + Style.RESET_ALL)
+
+    # Print the password
+    if (not args.quiet):
+        print("The generated password is: " + Fore.GREEN+ "\n%s\n" % (password) + Style.RESET_ALL)
+    else:
+        print(password)
+
 
 
 if __name__ == "__main__":
